e3d5cc9ae2e25184f251333afa13047f54ed1d1d
Sergey Punkoff <svpmailbox@gmail.com>
2017-11-21 23:02:04 +0300

    init

---
+++ setup.py
@@ -0,0 +1,32 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import os
+
+
+setup(
+    name='dlbot',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        #'virtualenv',
+        'flask',
+        #'PyYAML',
+        #'click',
+        #'pymongo',
+        #'psutil',
+        #'coloredlogs',
+        #'hgapi',
+        #'pillow',
+        #'humanfriendly',
+        #'colorama',
+        #'requests',
+    ],
+    #entry_points='''
+    #    [console_scripts]
+    #    {cli_tool_name}=cli.main:main
+    #'''.format(cli_tool_name=cli_tool_name),
+)
