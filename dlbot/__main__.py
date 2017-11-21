6b23e14fc3022189d7ba2e7e2b70434c11e9af99
Sergey Punkoff <svpmailbox@gmail.com>
2017-11-22 01:24:17 +0300

    configuration sources add

---
+++ dlbot/__main__.py
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/env python
+import sys
+import os
+
+if __name__ == '__main__':
+    ROOT_PATH = os.path.abspath(os.path.join(os.path.split(sys.argv[0])[0], '..'))
+    print(ROOT_PATH)
+    sys.path.append(ROOT_PATH)
+    from dlbot.dlbot import app
+    app.run()
+    
--- dlbot/dlbot.py
+++ dlbot/dlbot.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
+from dlbot import default_settings
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object(default_settings)
+app.config.from_pyfile('dlbot.cfg', silent=True)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run()
